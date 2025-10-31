b041744440ce3fecb1776c1488b13c43972664d1
safouane sabiq <safouane.sabiq@gmail.com>
2025-10-31 17:15:05 +0100

    TP JavaScript - tp-produits

---
+++ data/products.js
@@ -0,0 +1,40 @@
+export const PRODUCTS = [
+  {
+    id: 1,
+    name: "Lait UHT",
+    category: "Alimentaire",
+    price: 12.5,
+    currency: "MAD",
+    quantity: 20,
+    expiryDate: "2025-10-20",
+    tags: ["frais", "lactose"]
+  },{
+    id: 4,
+    name: "ahmed",
+    category: "Alimentaire",
+    price: 13.5,
+    currency: "MAD",
+    quantity: 45,
+    expiryDate: "2025-10-01",
+    tags: ["frais", "lactose"]
+  },{
+    id: 3,
+    name: "salma",
+    category: "Alimentaire",
+    price: 3.5,
+    currency: "MAD",
+    quantity: 100,
+    expiryDate: "2025-11-20",
+    tags: ["frais", "lactose"]
+  },{
+    id: 2,
+    name: "maryam",
+    category: "Alimentaire",
+    price: 5.5,
+    currency: "MAD",
+    quantity: 30,
+    expiryDate: "2025-4-12",
+    tags: ["frais", "lactose"]
+  }
+ 
+];
